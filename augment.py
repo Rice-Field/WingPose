c7f295e9176bf48247685212d2fddfd6bad55b1b
Jonathon Rice <jonrice@knights.ucf.edu>
2019-03-08 17:09:05 -0500

    Add files via upload

--- augment.py
+++ augment.py
@@ -1,94 +1,96 @@
-import os
-import glob
+import imgaug as ia
+from imgaug import augmenters as iaa
+import random
 import numpy as np
 import pandas as pd
-import cv2 as cv
-import imutils
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from PIL import Image, ImageFile
+import time
+ia.seed(int((time.time()*1000)%100000))
 
-# start with simple augmentations
-# 90* rotations, flips, noise and smoothing
+oimg_h = 1600
+oimg_w = 1200
+nimg_h = 256
+nimg_w = 256
 
-imgdata = np.load("imgdataC_256.npy")
-# hm = np.load("heatmap8_s1_64.npy")
-hm = np.load("heatmap8_s3_128.npy")
+def toSmall(points):
 
-nimgdata = np.zeros((3088,256,256,3), dtype=np.float32)
-nhm = np.zeros((3088,128,128,8), dtype=np.float32)
+    for k in range(len(points)):
+        for i in range(0,16,2):
+            points[k][i] = round(points[k][i]/oimg_h * nimg_h)
+            points[k][i+1] = round(points[k][i+1]/oimg_w * nimg_w)
 
-# are hm channels flipping correctly?
-j = 0
-for i in range(0,1544,8):
-	# print(j)
-	nimgdata[i] = imgdata[j]
-	nimgdata[i+1] = np.fliplr(nimgdata[i])
-	nhm[i] = hm[j]
-	nhm[i+1] = np.fliplr(nhm[i])
+    return points
 
-	# huge mess for flip and orig rotations, fix
-	
-	# nimgdata[i+2] = np.rot90(nimgdata[i])
-	# nimgdata[i+3] = np.rot90(nimgdata[i+1])
-	# nimgdata[i+4] = np.rot90(nimgdata[i+2])
-	# nimgdata[i+5] = np.rot90(nimgdata[i+3])
-	# nimgdata[i+6] = np.rot90(nimgdata[i+4])
-	# nimgdata[i+7] = np.rot90(nimgdata[i+5])
+def pointCoord(numdata):
+    points = []
+    for k in range(len(numdata)):
+        points.append([])
+        for i in range(0,16,2):
+            points[-1].append([numdata[k][i], numdata[k][i+1]])
 
-	nhm[i+2] = np.rot90(nhm[i])
-	nhm[i+3] = np.rot90(nhm[i+1])
-	nhm[i+4] = np.rot90(nhm[i+2])
-	nhm[i+5] = np.rot90(nhm[i+3])
-	nhm[i+6] = np.rot90(nhm[i+4])
-	nhm[i+7] = np.rot90(nhm[i+5])
-	j += 1
+    return points
 
-for i in range(1544,3088):
-	# nimgdata[i] += nimgdata[i-1544] + np.random.normal(0,1,(256,256,3))
-	nhm[i] += nhm[i-1544]
+def normalize(array):
+    maxim = np.max(array)
+    array /= maxim
+    return array
 
-# for i in range(8):
-# 	plt.imshow(nimgdata[i].astype(np.uint8))
-# 	plt.show()
+# save filepath to variable for easier access
+path = 'truewing.csv'
+# read the data and store data in DataFrame titled data
+data = pd.read_csv(path)
+points = ['x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4', 'x5', 'y5',
+                 'x6', 'y6', 'x7', 'y7', 'x8', 'y8']
+name = ['filename']
+X = data[points]
+files = data[name]
+numdata = X.values.tolist()
+filenames = files.values.tolist()
+# print(filenames[0])
 
-# for i in range(8):
-# 	plt.imshow(nimgdata[i+1544].astype(np.uint8))
-# 	plt.show()
+numdata = np.asarray(numdata)
+numdata = toSmall(numdata)
+points = pointCoord(numdata)
 
-# np.save("AugimgdataC_256", nimgdata)
-np.save("Augheatmap8_s3_128", nhm)
+# images = np.random.randint(0, 100, (4, 128, 128, 3), dtype=np.uint8)
+# images = np.random.uniform(0,1,[4, 128, 128, 3]).astype(np.float32)
+images = np.load("imgdataC_256-3.npy")
+images = images[0:4]/255
 
-# for i in range(len(hm),len(hm)*2):
-# 	nimgdata[i] = np.fliplr(imgdata[i])
+heatmaps = np.load("heatmap8_s3_128_R.npy")
+heatmaps = heatmaps[0:4, :, :, 0:3]
 
-# for i in range(len(hm)*2,len(nhm)):
-# 	nimgdata[i] = np.rot90(imgdata[i])
+# Generate random keypoints.
+# The augmenters expect a list of imgaug.KeypointsOnImage.
+i = 0
+keypoints_on_images = []
+for image in images:
+    height, width = image.shape[0:2]
+    keypoints = []
+    for k in range(8):
+        x = points[i][k][0]
+        y = points[i][k][1]
+        keypoints.append(ia.Keypoint(x=x, y=y))
+    keypoints_on_images.append(ia.KeypointsOnImage(keypoints, shape=image.shape))
+    i += 1
 
-# img = np.rot90(imgdata[0])
-# hm1 = np.rot90(hm[0,:,:,0])
+seq = iaa.Sequential([
+    # iaa.GaussianBlur((0, 0.1)), 
+    iaa.Crop(px=(0, 10)), # crop images from each side by 0 to 16px (randomly chosen)
+    iaa.Fliplr(0.5), # horizontally flip 50% of the images
+    iaa.Affine(scale=(0.75, 1.25), translate_percent={"x": (-0.25, 0.25), "y": (-0.25, 0.25)}, rotate=(-30, 30)),
+    # iaa.Pad(pad_mode=ia.ALL, pad_cval=(0, 255))
+    ])
 
-# img = imgdata[0]
-# img = np.fliplr(imgdata[0])
-# hm1 = np.fliplr(hm[0,:,:,0])
+seq_det = seq.to_deterministic() # call this for each batch again, NOT only once at the start
 
-# plt.imshow(img.astype(np.uint8))
-# plt.show()
-# plt.imshow(hm1)
-# plt.show()
+# augment keypoints and images
+images_aug = seq_det.augment_images(images) 
+keypoints_aug = seq_det.augment_keypoints(keypoints_on_images)
+heatmaps_aug = seq_det.augment_images(heatmaps) 
 
-
-
-
-# Show flip and rotations
-#########################
-# fig = plt.figure(figsize=(8,8))
-# columns = 4
-# rows = 2
-# for i in range(1, columns*rows+1):
-# 	if i == 5:
-# 		img = np.fliplr(img)
-# 	fig.add_subplot(rows, columns, i)
-# 	plt.imshow(img.astype(np.uint8))
-# 	img = np.rot90(img)
-# plt.show()
+# Example code to show each image and print the new keypoints coordinates
+for img_idx, (image_before, image_after, keypoints_before, keypoints_after, heatmap_before, heatmap_after) in enumerate(zip(images, images_aug, keypoints_on_images, keypoints_aug, heatmaps, heatmaps_aug)):
+    image_before = keypoints_before.draw_on_image(image_before)
+    image_after = keypoints_after.draw_on_image(image_after)
+    ia.imshow(np.concatenate((image_before, image_after), axis=1)) # before and after
+    # ia.imshow(np.concatenate((normalize(heatmap_before), normalize(heatmap_after)), axis=1))
